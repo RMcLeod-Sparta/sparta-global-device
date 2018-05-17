be37246d761548d1b9ff39f7810236b628c25eae
Tech-A06 <tech-a06@tech-a06s-air.home>
2018-05-17 23:57:27 +0100

    hopefully a working version

--- db/schema.rb
+++ db/schema.rb
@@ -25,10 +25,8 @@ ActiveRecord::Schema.define(version: 2018_05_17_112827) do
     t.string "title"
     t.string "body"
     t.integer "rating"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,5 +48,4 @@ ActiveRecord::Schema.define(version: 2018_05_17_112827) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "posts", "users"
 end
